917d308a3d09830bd02396f01decbd4858dfd6c5
Chris Rodriguez <info@premiumwd.com>
2018-10-24 16:36:46 -0700

    styling home

--- src/app/layout/pages/home/home.component.ts
+++ src/app/layout/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Title, Meta } from "@angular/platform-browser";
 
 // COMPONENT
 @Component({
@@ -9,7 +10,12 @@ import { Component, OnInit } from "@angular/core";
 // CLASS
 export class HomeComponent implements OnInit {
 
-  constructor(){}
+  constructor(title: Title, meta: Meta){
+		title.setTitle('Drag and Drop Interface - Vicky\'s');
+		meta.addTags([
+			{ name: 'description', content: 'Making it simple and easy drag and drop products into cart.' },
+		]);
+  }
   ngOnInit(): void {
   }
 }
